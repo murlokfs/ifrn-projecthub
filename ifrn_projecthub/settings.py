6d3e274c337d99b34b371d5d545a16eafbdd7213
murlokfs <murilokauan@hotmail.com>
2025-10-22 20:50:47 -0300

    setup django + creating apps

--- ifrn_projecthub/settings.py
+++ ifrn_projecthub/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
+    'project',
+    'authentication',
+
+    # Libraries
 ]
 
 MIDDLEWARE = [
@@ -102,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
