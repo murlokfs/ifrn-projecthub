e936e32441e512e558f2cf0e03b16f0f3805a9e5
Eduardo <oedu2303@gmail.com>
2025-11-01 14:30:23 -0300

    Adicionando a componentização do card Projeo

--- project/views.py
+++ project/views.py
@@ -1,19 +1,14 @@
-# views.py
-
 from django.shortcuts import render
-# from authentication.models import User # Not used, can be removed
-# from project.models import Projeto # Not used, can be removed
 
 def my_projects(request):
-    # Create a list of the desired length (e.g., [1, 2, 3])
-    # The actual content of the list doesn't matter, only its length
-    # A list comprehension is a clean way to do this.
     num_iterations = 4
-    contador_list = range(num_iterations) # range(3) creates a range object from 0 to 2
+    contador_list = range(num_iterations)
+    show_status = True
 
     context = {
-        "contador": num_iterations, # Keep this if you need the number itself for something else
-        "contador_list": contador_list # The iterable to loop over
+        "contador": num_iterations,
+        "contador_list": contador_list,
+        "show_status": show_status,
     }
 
     return render(request, 'project/my_projects.html', context)
