ef85b24220a2ec3a4e8589aa8115fb1267c0fbfa
Eduardo <oedu2303@gmail.com>
2025-11-11 13:56:25 -0300

    Reformulando a estrutura do card projeto

--- project/urls.py
+++ project/urls.py
@@ -4,5 +4,5 @@ from project import views
 urlpatterns = [
     path('', views.feed, name='index'),
     path('projects/', views.my_projects, name='my_projects'),
-    path('popup/', views.popup, name='popup'),
+    # path('create_project/', views.create_project, name='create_project'),
 ]
--- project/views.py
+++ project/views.py
@@ -17,14 +17,16 @@ def my_projects(request):
     num_iterations = 4
     contador_list = range(num_iterations)
     show_status = True
+    show_actions = True
 
     context = {
-        "contador": num_iterations,
+        "contador": num_iterations, 
         "contador_list": contador_list,
         "show_status": show_status,
+        "show_actions": show_actions,
     }
 
     return render(request, 'project/my_projects.html', context)
 
-def popup(request):
-    return render(request, 'components/modals/awaiting_approval.html')
+# def create_project(request):
+#     return render(request, 'project/create_project.html')
