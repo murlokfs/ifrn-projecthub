b669c1e97f29e6aa50edc24905f5bfc3306f10c4
Ayrton Cav <franciscoayrton123@gmail.com>
2025-11-15 18:05:38 -0300

    header-finalizado

--- authentication/views.py
+++ authentication/views.py
@@ -4,3 +4,5 @@ from django.views.generic import TemplateView
 
 class PerfilView(TemplateView):
     template_name = 'authentication/perfil.html'
+    # passa active_page para que a sidebar marque a rota correta
+    extra_context = {'active_page': 'perfil'}
