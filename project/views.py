b30ce6a58c90154ab61a1a9983e4840be8b1c46d
Nathan Manicoba <zaphiroxd5@gmail.com>
2025-11-19 10:37:20 -0300

    Fazendo os cards terem cores diferentes de acordo com o estatus

--- project/views.py
+++ project/views.py
@@ -17,18 +17,19 @@ def feed(request):
     return render(request, 'project/feed.html', context)
 
 def my_projects(request):
-    num_iterations = 4
-    contador_list = range(num_iterations)
-    show_status = True
+
+    projetos = [
+        {"status": "aprovado"},
+        {"status": "pendente"},
+        {"status": "reprovado"},
+        {"status": "aprovado"},
+    ]
 
     context = {
-        "contador": num_iterations,
-        "contador_list": contador_list,
-        "show_status": show_status,
+        "projetos": projetos,
         "active_page": "my_projects",
     }
-
-    return render(request, 'project/my_projects.html', context)
+    return render(request, "project/my_projects.html", context)
 
 class DetalhesProjetosView(TemplateView):
     template_name = 'project/detalhes_projetos.html'
