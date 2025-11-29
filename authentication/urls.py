9fc726518b0d53c728cfe3b2a682ad0949ab574c
Ayrton Cav <franciscoayrton123@gmail.com>
2025-11-29 17:45:50 -0300

    padronizando-arquivos

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from authentication.views import *
 
 urlpatterns = [
-    path('perfil/', PerfilView.as_view(), name='perfil' ),
+    path('profile/', PerfilView.as_view(), name='profile' ),
     path('login/', LoginView.as_view(), name='login'),
     path('signup/', SignupView.as_view(), name='signup'),
 ]
--- static/base_files/js/create_project.js
+++ static/base_files/js/create_project.js
@@ -1,16 +1,12 @@
 // static/components/base_files/create_project.js
 document.addEventListener('click', (e) => {
-  
   const btn = e.target.closest('.create_project');
   if (!btn) return;
-
   const modalBackdrop = document.querySelector('#types-projects-modal');
   if (modalBackdrop) {
     modalBackdrop.classList.add('visible');
     return;
   }
-
-  // Se modal ainda não está no DOM, dispara um evento global que o modal pode ouvir
   const evt = new CustomEvent('openCreateProjectModal', { bubbles: true });
   document.dispatchEvent(evt);
 });
