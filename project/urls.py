b8e55609d7601dc7afd6e2f6fd9c8a479117a5b2
Nathan Manicoba <zaphiroxd5@gmail.com>
2026-01-25 18:23:15 -0300

    Back end e filtro feito

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from project import views
-from .views import DetalhesProjetosView, ComentariosAlunosView, ComentariosProfessoresView, ProjetosAprovacaoView
+from .views import DetalhesProjetosView, ComentariosAlunosView, ComentariosProfessoresView, ProjetosAprovacaoView,FeedView,SearchView
 
 urlpatterns = [
 
-    path('project-details/', DetalhesProjetosView.as_view(), name='project_details'),
+    path('project-details/<int:pk>/', DetalhesProjetosView.as_view(), name='project_details'),
     path('student-comments/', ComentariosAlunosView.as_view(), name='student_comments'),
     path('create-project/', views.CadastroProjetoView.as_view(), name='create_project'),
     path('teacher-comments/', ComentariosProfessoresView.as_view(), name='teacher_comments'),
     path('project-approval/', ProjetosAprovacaoView.as_view(), name='project_approval'),
-    path('', views.feed, name='index'),
+    path('', FeedView.as_view(), name='index'),
     path('projects/', views.my_projects, name='my_projects'),
+    path('search/', SearchView.as_view(), name='search'),
 ]
